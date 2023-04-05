15d4ff0ed9b06c84204b6b83a53991e108b884b3
Kassidy <kassidyjo97@gmail.com>
2023-04-05 15:28:24 -0500

    add file path and theme name to output

--- install.php
+++ install.php
@@ -4,21 +4,16 @@
 
   $url = 'https://raw.githubusercontent.com/callmekass/pfSense-palenight/main/callmekass-palenight.css';
 
-  // Use basename() function to return the base name of file
   $file_name = basename($url);
-
-  echo $file_name;
+  $file_path = '/usr/local/www/css/'.$file_name;
   
-  // Use file_get_contents() function to get the file
-  // from url and use file_put_contents() function to
-  // save the file by using base name
-  if (file_put_contents($file_name, file_get_contents($url)))
+  // Get file from url and save to css themes folder
+  if (file_put_contents($file_path, file_get_contents($url)))
   {
-      echo "File downloaded successfully\n";
+      echo 'Theme installed successfully - '.$file_name.'\n';
   }
   else
   {
-      echo "File downloading failed.\n";
+      echo 'Failed to download theme - '.$file_name.'\n';
   }
-
 ?>
