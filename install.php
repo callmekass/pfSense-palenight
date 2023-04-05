942365b969440b995a3471862237350464c742fc
Kassidy <kassidyjo97@gmail.com>
2023-04-05 16:12:56 -0500

    removed special characters because they aren't supported in the webconfigurator cli

--- install.php
+++ install.php
@@ -1,9 +1,6 @@
 <?php
   echo "Intializing theme installation\n";
 
-  $symbol_check = "\e[0;32m\xe2\x9c\x93\e[0m";
-  $symbol_cross = "\e[0;31m\xe2\x9c\x95\e[0m";
-
   $url = 'https://raw.githubusercontent.com/callmekass/pfSense-palenight/main/callmekass-palenight.css';
 
   $file_name = basename($url);
@@ -12,10 +9,10 @@
   // Get file from url and save to css themes folder
   if (file_put_contents($file_path, file_get_contents($url)))
   {
-      echo $symbol_check.' '.$file_name.' installed successfully.'."\n";
+      echo $file_name.' installed successfully.'."\n";
   }
   else
   {
-      echo $symbol_cross.' '.$file_name.' failed to download.'."\n";
+      echo $file_name.' failed to download.'."\n";
   }
 ?>
