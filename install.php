309776711ee98067052d1486daaf20315b29c98f
Kassidy <kassidyjo97@gmail.com>
2023-04-05 15:09:58 -0500

    add file download to script

--- install.php
+++ install.php
@@ -1,3 +1,24 @@
 <?php
-  echo "Hello PHP\n"
+  echo "Intializing theme installation";
+
+
+  $url = 'https://raw.githubusercontent.com/callmekass/pfSense-palenight/main/callmekass-palenight.css';
+  
+  // Use basename() function to return the base name of file
+  $file_name = basename($url);
+      
+  // Use file_get_contents() function to get the file
+  // from url and use file_put_contents() function to
+  // save the file by using base name
+  if (file_put_contents($file_name, file_get_contents($url)))
+  {
+      echo "File downloaded successfully";
+  }
+  else
+  {
+      echo "File downloading failed.";
+  }
+
+
+
 ?>
