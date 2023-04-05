eb6cb874987405c2453f1585b07c468786cd2f24
Kassidy <kassidyjo97@gmail.com>
2023-04-05 15:14:56 -0500

    display filename for debugging

--- install.php
+++ install.php
@@ -3,22 +3,22 @@
 
 
   $url = 'https://raw.githubusercontent.com/callmekass/pfSense-palenight/main/callmekass-palenight.css';
-  
+
   // Use basename() function to return the base name of file
   $file_name = basename($url);
-      
+
+  echo $file_name
+  
   // Use file_get_contents() function to get the file
   // from url and use file_put_contents() function to
   // save the file by using base name
   if (file_put_contents($file_name, file_get_contents($url)))
   {
-      echo "File downloaded successfully";
+      echo "File downloaded successfully\n";
   }
   else
   {
-      echo "File downloading failed.";
+      echo "File downloading failed.\n";
   }
-
-
 
 ?>
