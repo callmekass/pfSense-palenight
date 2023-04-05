7fa8aaaa9ef6bd1141dfa0e98387071af9bb7ddc
Kassidy <kassidyjo97@gmail.com>
2023-04-05 15:51:07 -0500

    add colored symbols to success and failure message

--- install.php
+++ install.php
@@ -1,5 +1,9 @@
 <?php
-  echo "###Intializing theme installation\n";
+
+  $symbol_check "\e[0;32m\xe2\x9c\x93\e[0m";
+  $symbol_cross "\e[0;31m\xe2\x9c\x95\e[0m";
+
+  echo "Intializing theme installation\n";
 
 
   $url = 'https://raw.githubusercontent.com/callmekass/pfSense-palenight/main/callmekass-palenight.css';
@@ -10,10 +14,10 @@
   // Get file from url and save to css themes folder
   if (file_put_contents($file_path, file_get_contents($url)))
   {
-      echo 'Theme installed successfully - '.$file_name."\n";
+      echo $symbol_check.' - Theme installed successfully - '.$file_name."\n";
   }
   else
   {
-      echo 'Failed to download theme - '.$file_name."\n";
+      echo $symbol_cross.' - Failed to download theme - '.$file_name."\n";
   }
 ?>
