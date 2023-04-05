de6dcc04f05f60b435cc833d78de792f20370c7c
Kassidy <kassidyjo97@gmail.com>
2023-04-05 15:57:02 -0500

    reformated output messages

--- install.php
+++ install.php
@@ -1,10 +1,8 @@
 <?php
+  echo "Intializing theme installation\n";
 
   $symbol_check = "\e[0;32m\xe2\x9c\x93\e[0m";
   $symbol_cross = "\e[0;31m\xe2\x9c\x95\e[0m";
-
-  echo "Intializing theme installation\n";
-
 
   $url = 'https://raw.githubusercontent.com/callmekass/pfSense-palenight/main/callmekass-palenight.css';
 
@@ -14,10 +12,10 @@
   // Get file from url and save to css themes folder
   if (file_put_contents($file_path, file_get_contents($url)))
   {
-      echo $symbol_check.' - Theme installed successfully - '.$file_name."\n";
+      echo $symbol_check.' '.$file_name.' installed successfully.'."\n";
   }
   else
   {
-      echo $symbol_cross.' - Failed to download theme - '.$file_name."\n";
+      echo $symbol_cross.' '.$file_name.' failed to download.'."\n";
   }
 ?>
